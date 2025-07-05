5da449a4d7fbd1b06fec6f46f4ff90c5ea2d0e90
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-05 12:18:55 -0500

    Auto-save-__init__

--- ob_model/v2.0_precloud_reorganization_and_clean/__init__.py
+++ ob_model/v2.0_precloud_reorganization_and_clean/__init__.py
@@ -12,7 +12,7 @@ Regime System - Institutional Grade Trading Analysis
 A sophisticated regime classification and optimization system
 """
 
-# .gitignore 2
+# .gitignore 5
 
 __version__ = '1.0.0'
 __author__ = 'Regime System Team'
