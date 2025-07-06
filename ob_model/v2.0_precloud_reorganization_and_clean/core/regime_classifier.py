c92c06e797e4e08dd1f942cd37091f6c2fe246e5
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-06 15:48:31 -0500

    Auto-save-regime_classifier

--- ob_model/v2.0_precloud_reorganization_and_clean/core/regime_classifier.py
+++ ob_model/v2.0_precloud_reorganization_and_clean/core/regime_classifier.py
@@ -1,20 +1,8 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:percent
-#     text_representation:
-#       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.17.2
-#   kernelspec:
-#     display_name: Python 3 (ipykernel)
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
-# %%
-# RUNONSAVE TEST
+# In[ ]:
+
 
 """
 Core regime classification components
@@ -697,3 +685,4 @@ class RollingRegimeClassifier:
             }
         
         return stats
+
