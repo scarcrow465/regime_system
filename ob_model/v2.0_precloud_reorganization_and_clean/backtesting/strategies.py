0b5b9cc5f28ef699551fb671627efc295ef1e0e5
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-08 08:08:14 -0500

    Auto-save-strategies

--- ob_model/v2.0_precloud_reorganization_and_clean/backtesting/strategies.py
+++ ob_model/v2.0_precloud_reorganization_and_clean/backtesting/strategies.py
@@ -255,7 +255,7 @@ class EnhancedRegimeStrategyBacktester:
                 slow_ma = data['close'].rolling(config.momentum_slow_period).mean()
                 
                 if 'MACD' in data.columns:
-                    macd_signal = data['MACD'] > data['MACD_Signal']
+                    macd_signal = data['MACD'] > data['MACD_signal']
                 else:
                     macd_signal = pd.Series(True, index=data.index)
                 
