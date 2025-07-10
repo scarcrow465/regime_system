f7c76432fc17455ff86d2be40fff27e330a62f0c
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-09 19:19:16 -0500

    Auto-save-daily_regime_classifier

--- ob_model/v2.0_precloud_reorganization_and_clean/daily_regime_classifier.py
+++ ob_model/v2.0_precloud_reorganization_and_clean/daily_regime_classifier.py
@@ -65,7 +65,7 @@ class NQDailyRegimeClassifier:
 
         self.thresholds = {
             'direction_strong': 0.3,  # Threshold for Strong Uptrend/Downtrend
-            'direction_neutral': 0.05, # Threshold for Neutral Sideways
+            'direction_neutral': 0.03, # Threshold for Neutral Sideways
             'strength_strong': 0.325,   # Threshold for Strong
             'strength_moderate': 0.2, # Threshold for Moderate
             'vol_low': 35,
