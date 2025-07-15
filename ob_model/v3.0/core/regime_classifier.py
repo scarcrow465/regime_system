3e33b3fc26e407111cbaf0a27a2491c65a0037f9
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-15 18:07:33 -0500

    Auto-save-regime_classifier

--- ob_model/v3.0/core/regime_classifier.py
+++ ob_model/v3.0/core/regime_classifier.py
@@ -22,18 +22,19 @@ def add_session_labels(df):
         log_message("Empty DF for session labels", 'error')
         return df
     df['hour'] = df.index.hour
-    # Full sessions (can overlap)
+    # Full sessions (can overlap) - The all need fixing to be correct - FIX THIS
     df['full_session'] = 'Other'
-    df.loc[(df['hour'] >= 18) | (df['hour'] < 2), 'full_session'] = 'Asia'  # 6pm-2am ET
-    df.loc[(df['hour'] >= 2) & (df['hour'] < 8), 'full_session'] = 'London'  # 2-8am (overlaps NY open at 8)
-    df.loc[(df['hour'] >= 8) & (df['hour'] < 16), 'full_session'] = 'NY'  # 8am-4pm (overlaps London close)
+    df.loc[(df['hour'] >= 18) | (df['hour'] < 2), 'full_session'] = 'Sydney'  # 6pm-2am ET
+    df.loc[(df['hour'] >= 20) | (df['hour'] < 5), 'full_session'] = 'Tokyo'  # 6pm-2am ET
+    df.loc[(df['hour'] >= 3) & (df['hour'] < 12), 'full_session'] = 'London'  # 2-8am (overlaps NY open at 8)
+    df.loc[(df['hour'] >= 7) & (df['hour'] < 16), 'full_session'] = 'NY'  # 8am-4pm (overlaps London close)
     
     # Refined sessions (no overlap between them, within full)
     df['refined_session'] = 'Other'
-    df.loc[(df['hour'] >= 18) & (df['hour'] < 20), 'refined_session'] = 'Asia_Open'  # Example sub: 6-8pm
-    df.loc[(df['hour'] >= 2) & (df['hour'] < 4), 'refined_session'] = 'London_Open'  # 2-4am
-    df.loc[(df['hour'] >= 8) & (df['hour'] < 11), 'refined_session'] = 'NY_Open'  # 8-11am
-    df.loc[(df['hour'] >= 11) & (df['hour'] < 15), 'refined_session'] = 'NY_Afternoon'  # 11am-3pm
+    df.loc[(df['hour'] >= 20) & (df['hour'] < 0), 'refined_session'] = 'Asia_Open'  # Example sub: 6-8pm
+    df.loc[(df['hour'] >= 2) & (df['hour'] < 5), 'refined_session'] = 'London_Open'  # 2-4am
+    df.loc[(df['hour'] >= 7) & (df['hour'] < 12), 'refined_session'] = 'NY_Open'  # 8-11am
+    df.loc[(df['hour'] >= 13) & (df['hour'] < 15), 'refined_session'] = 'NY_Afternoon'  # 11am-3pm
     df.loc[(df['hour'] >= 15) & (df['hour'] < 16), 'refined_session'] = 'Power_Hour'  # 3-4pm
     
     if DEBUG_LEVEL == 'verbose':
