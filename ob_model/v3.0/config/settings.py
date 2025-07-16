e3cb4e8b717212f21e246418133863c6ac47f373
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-16 06:22:24 -0500

    Auto-save-settings

--- ob_model/v3.0/config/settings.py
+++ ob_model/v3.0/config/settings.py
@@ -18,8 +18,8 @@ START_DATE = '2008-01-01 00:00:00-05:00'
 END_DATE = '2025-04-02 00:00:00-04:00'  # Current date
 
 DATA_PATH = [
-    r"C:\Users\rs\OneDrive\Desktop\Excel\Data\New Data\7.1 Master 15m Data - Updated - Nearest Unadjusted - 2014_01_01 - 2025_04_01 .csv",
-    r"C:\Users\rs\OneDrive\Desktop\Excel\Data\New Data\7.2 Master 15m Data - Updated - Nearest Unadjusted - 2000_01_01 - 2013_12_31 .csv"
+    r"C:\Users\rs\OneDrive\Desktop\Excel\Data\New Data\7.2 Master 15m Data - Updated - Nearest Unadjusted - 2000_01_01 - 2013_12_31 .csv",
+    r"C:\Users\rs\OneDrive\Desktop\Excel\Data\New Data\7.1 Master 15m Data - Updated - Nearest Unadjusted - 2014_01_01 - 2025_04_01 .csv"
 ]
 
 # Dirs (already created, but for reference)
