5b9cc0dd70469fd733cce83b377c48e7464ee7e1
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-30 02:38:25 -0500

    Auto-save-ma_regime_system_v2

--- ob_model/v2.0_precloud_reorganization_and_clean/ma_regime_system_v2.py
+++ ob_model/v2.0_precloud_reorganization_and_clean/ma_regime_system_v2.py
@@ -12,7 +12,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # Centralized parameters
-TEST_SLICE = 20000  # Number of rows to use from the end of the dataset (set to None for full dataset)
+TEST_SLICE = None  # Number of rows to use from the end of the dataset (set to None for full dataset)
 DATA_FILE = 'combined_NQ_15m_data.csv'  # Path to your CSV file
 OUTPUT_FILE = 'ma_regime_labeled_data_with_perfect_v15.csv'  # Output CSV file name
 SCORING_FILE = 'regime_scoring_metrics_v15.csv'  # Scoring metrics output
