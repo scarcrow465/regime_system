a4144caefd94789b684350ff7af26f66a7f443d2
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-30 22:45:03 -0500

    Auto-save-ma_regime_system_v5

--- ob_model/v2.0_precloud_reorganization_and_clean/ma_regime_system_v5.py
+++ ob_model/v2.0_precloud_reorganization_and_clean/ma_regime_system_v5.py
@@ -14,8 +14,8 @@ warnings.filterwarnings('ignore')
 # Centralized parameters
 TEST_SLICE = 5000
 DATA_FILE = 'combined_NQ_15m_data.csv'
-OUTPUT_FILE = 'ma_regime_labeled_data_with_perfect_v37_test.csv'
-SCORING_FILE = 'regime_scoring_metrics_v37_test.csv'
+OUTPUT_FILE = 'ma_regime_labeled_data_with_perfect_v38_test.csv'
+SCORING_FILE = 'regime_scoring_metrics_v38_test.csv'
 TIMEFRAME = '15min'
 
 ENHANCED_FEATURES = True
@@ -50,8 +50,8 @@ ENHANCED_PARAMS = {
     'weak_persistence': 2,
     'between_persistence': 3,
     'session_adaptation': False,
-    'between_scale_sensitivity': 2.0,
-    'volatility_scale_factor': 2.0
+    'between_scale_sensitivity': 3.0,
+    'volatility_scale_factor': 1.0
 }
 
 PERFECT_PARAMS = {
