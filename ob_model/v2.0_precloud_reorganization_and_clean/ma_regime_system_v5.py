90133cac065fe8cc6317b7afea29c00a7eedb2cc
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-31 22:45:44 -0500

    Auto-save-ma_regime_system_v5

--- ob_model/v2.0_precloud_reorganization_and_clean/ma_regime_system_v5.py
+++ ob_model/v2.0_precloud_reorganization_and_clean/ma_regime_system_v5.py
@@ -12,10 +12,10 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # Centralized parameters
-TEST_SLICE = 5000
+TEST_SLICE = None
 DATA_FILE = 'combined_NQ_15m_data.csv'
-OUTPUT_FILE = 'ma_regime_labeled_data_with_perfect_v62_test.csv'
-SCORING_FILE = 'regime_scoring_metrics_v62_test.csv'
+OUTPUT_FILE = 'ma_regime_labeled_data_with_perfect_v63_test.csv'
+SCORING_FILE = 'regime_scoring_metrics_v63_test.csv'
 TIMEFRAME = '15min'
 
 ENHANCED_FEATURES = True
@@ -30,7 +30,7 @@ CORE_PARAMS = {
     'bull_strong_percentile': 0.75,
     'bear_weak_percentile': 0.50,
     'bear_strong_percentile': 0.25,
-    'volatility_lookback': 64,
+    'volatility_lookback': 32,
     'volatility_filter_min': 0.5,
     'volatility_filter_max': 1.3,
     'volatility_low_percentile': 0.25,
