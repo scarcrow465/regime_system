23cb6567b30a9e33b98b3f50d8d3a18da48d994f
scarcrow465 <166575471+scarcrow465@users.noreply.github.com>
2025-07-31 22:36:06 -0500

    Auto-save-ma_regime_system_v5

--- ob_model/v2.0_precloud_reorganization_and_clean/ma_regime_system_v5.py
+++ ob_model/v2.0_precloud_reorganization_and_clean/ma_regime_system_v5.py
@@ -14,25 +14,25 @@ warnings.filterwarnings('ignore')
 # Centralized parameters
 TEST_SLICE = 5000
 DATA_FILE = 'combined_NQ_15m_data.csv'
-OUTPUT_FILE = 'ma_regime_labeled_data_with_perfect_v61_test.csv'
-SCORING_FILE = 'regime_scoring_metrics_v61_test.csv'
+OUTPUT_FILE = 'ma_regime_labeled_data_with_perfect_v62_test.csv'
+SCORING_FILE = 'regime_scoring_metrics_v62_test.csv'
 TIMEFRAME = '15min'
 
 ENHANCED_FEATURES = True
 
 CORE_PARAMS = {
     'short_ma_period': 2,
-    'long_ma_period': 8,
+    'long_ma_period': 16,
     'short_atr_period': 2,
-    'long_atr_period': 8,
-    'base_slope_lookback': 16,
+    'long_atr_period': 16,
+    'base_slope_lookback': 32,
     'bull_weak_percentile': 0.50,
     'bull_strong_percentile': 0.75,
     'bear_weak_percentile': 0.50,
     'bear_strong_percentile': 0.25,
-    'volatility_lookback': 32,
+    'volatility_lookback': 64,
     'volatility_filter_min': 0.5,
-    'volatility_filter_max': 1.5,
+    'volatility_filter_max': 1.3,
     'volatility_low_percentile': 0.25,
     'volatility_high_percentile': 0.75,
     'volatility_high_bull_percentile': 0.75,
